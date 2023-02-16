07bc63f897da54704c1d816f66cccc0ca5ee6875
xDB-9 <84173278+xDB-9@users.noreply.github.com>
2023-02-16 23:02:12 +0800

    Add files via upload

---
+++ Analysis/Pre-Processing/Upcoming_Artists.R
@@ -0,0 +1,273 @@
+################Pre-Processing the Data
+
+###Austria
+
+#Null check
+isTRUE(is.na(upcoming_artists_austria_list))
+isTRUE(is.null(upcoming_artists_austria_list))
+
+#Data Format check
+class(upcoming_artists_austria_list)
+for(i in 1:length(upcoming_artists_austria_list))
+{
+  if(class(upcoming_artists_austria_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Brazil
+
+#Null check
+isTRUE(is.na(upcoming_artists_brazil_list))
+isTRUE(is.null(upcoming_artists_brazil_list))
+
+#Data Format check
+class(upcoming_artists_brazil_list)
+for(i in 1:length(upcoming_artists_brazil_list))
+{
+  if(class(upcoming_artists_brazil_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Brazil
+
+#Null check
+isTRUE(is.na(upcoming_artists_colombia_list))
+isTRUE(is.null(upcoming_artists_colombia_list))
+
+#Data Format check
+class(upcoming_artists_colombia_list)
+for(i in 1:length(upcoming_artists_colombia_list))
+{
+  if(class(upcoming_artists_colombia_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Colombia
+
+#Null check
+isTRUE(is.na(upcoming_artists_colombia_list))
+isTRUE(is.null(upcoming_artists_colombia_list))
+
+#Data Format check
+class(upcoming_artists_colombia_list)
+for(i in 1:length(upcoming_artists_colombia_list))
+{
+  if(class(upcoming_artists_colombia_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###France
+
+#Null check
+isTRUE(is.na(upcoming_artists_france_list))
+isTRUE(is.null(upcoming_artists_france_list))
+
+#Data Format check
+class(upcoming_artists_france_list)
+for(i in 1:length(upcoming_artists_france_list))
+{
+  if(class(upcoming_artists_france_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Germany
+
+#Null check
+isTRUE(is.na(upcoming_artists_germany_list))
+isTRUE(is.null(upcoming_artists_germany_list))
+
+#Data Format check
+class(upcoming_artists_germany_list)
+for(i in 1:length(upcoming_artists_germany_list))
+{
+  if(class(upcoming_artists_germany_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###India
+
+#Null check
+isTRUE(is.na(upcoming_artists_india_list))
+isTRUE(is.null(upcoming_artists_india_list))
+
+#Data Format check
+class(upcoming_artists_india_list)
+for(i in 1:length(upcoming_artists_india_list))
+{
+  if(class(upcoming_artists_india_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Indonesia
+
+#Null check
+isTRUE(is.na(upcoming_artists_indonesia_list))
+isTRUE(is.null(upcoming_artists_indonesia_list))
+
+#Data Format check
+class(upcoming_artists_indonesia_list)
+for(i in 1:length(upcoming_artists_indonesia_list))
+{
+  if(class(upcoming_artists_indonesia_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Japan
+
+#Null check
+isTRUE(is.na(upcoming_artists_japan_list))
+isTRUE(is.null(upcoming_artists_japan_list))
+
+#Data Format check
+class(upcoming_artists_japan_list)
+for(i in 1:length(upcoming_artists_japan_list))
+{
+  if(class(upcoming_artists_japan_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Mexico
+
+#Null check
+isTRUE(is.na(upcoming_artists_mexico_list))
+isTRUE(is.null(upcoming_artists_mexico_list))
+
+#Data Format check
+class(upcoming_artists_mexico_list)
+for(i in 1:length(upcoming_artists_mexico_list))
+{
+  if(class(upcoming_artists_mexico_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Netherlands
+
+#Null check
+isTRUE(is.na(upcoming_artists_netherlands_list))
+isTRUE(is.null(upcoming_artists_netherlands_list))
+
+#Data Format check
+class(upcoming_artists_netherlands_list)
+for(i in 1:length(upcoming_artists_netherlands_list))
+{
+  if(class(upcoming_artists_netherlands_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Panama
+
+#Null check
+isTRUE(is.na(upcoming_artists_panama_list))
+isTRUE(is.null(upcoming_artists_panama_list))
+
+#Data Format check
+class(upcoming_artists_panama_list)
+for(i in 1:length(upcoming_artists_panama_list))
+{
+  if(class(upcoming_artists_panama_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Philippines
+
+#Null check
+isTRUE(is.na(upcoming_artists_philippines_list))
+isTRUE(is.null(upcoming_artists_philippines_list))
+
+#Data Format check
+class(upcoming_artists_philippines_list)
+for(i in 1:length(upcoming_artists_philippines_list))
+{
+  if(class(upcoming_artists_philippines_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###SouthAfrica
+
+#Null check
+isTRUE(is.na(upcoming_artists_southafrica_list))
+isTRUE(is.null(upcoming_artists_southafrica_list))
+
+#Data Format check
+class(upcoming_artists_southafrica_list)
+for(i in 1:length(upcoming_artists_southafrica_list))
+{
+  if(class(upcoming_artists_southafrica_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###Spain
+
+#Null check
+isTRUE(is.na(upcoming_artists_spain_list))
+isTRUE(is.null(upcoming_artists_spain_list))
+
+#Data Format check
+class(upcoming_artists_spain_list)
+for(i in 1:length(upcoming_artists_spain_list))
+{
+  if(class(upcoming_artists_spain_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###United Kingdom
+
+#Null check
+isTRUE(is.na(upcoming_artists_uk_list))
+isTRUE(is.null(upcoming_artists_uk_list))
+
+#Data Format check
+class(upcoming_artists_uk_list)
+for(i in 1:length(upcoming_artists_uk_list))
+{
+  if(class(upcoming_artists_uk_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
+
+###United States of America
+
+#Null check
+isTRUE(is.na(upcoming_artists_us_list))
+isTRUE(is.null(upcoming_artists_us_list))
+
+#Data Format check
+class(upcoming_artists_us_list)
+for(i in 1:length(upcoming_artists_us_list))
+{
+  if(class(upcoming_artists_us_list[[i]]) == "data.frame")
+    next
+  else
+    print("ith element not a DataFrame")
+}
